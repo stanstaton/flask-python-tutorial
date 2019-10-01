982ae356f61502dbc53f9e1dc40f5fb9b20da80b
stanstaton <thatkidenzo@gmail.com>
2019-09-30 20:13:33 -0700

    implemented form and db

--- blueprints/operators.py
+++ blueprints/operators.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, Blueprint, request
+from flask import Flask, render_template, Blueprint, request, redirect
+from models.index import db
 
 operators_blueprint = Blueprint('operators', __name__, url_prefix="/operators")
 
@@ -6,3 +7,12 @@ operators_blueprint = Blueprint('operators', __name__, url_prefix="/operators")
 def index():
     if request.method == 'GET':
         return render_template('operators.html')
+    elif request.method == 'POST':
+        db.operators.insert_one({
+            'name': request.form['name'],
+            'uses': request.form['uses'],
+            'description': request.form['description'],
+            'symbol': request.form['symbol'],
+            'example': request.form['example']
+        })
+        return redirect('/operators')
