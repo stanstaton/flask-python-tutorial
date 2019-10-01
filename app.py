dc13df6381f4d4932f6cf85c54d367afcd18bf0a
stanstaton <thatkidenzo@gmail.com>
2019-09-30 18:32:07 -0700

    init commit

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # YOUR FLASK APP HERE
+#Imports
+from flask import Flask
+
+#Instantiate App
+app = Flask(__name__)
+
+#Home Route
+@app.route('/')
+def Home():
+    return 'MAIN PAGE STUB'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
